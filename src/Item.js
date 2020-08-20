dc3adc6f850198933364816bde54a9603cac0de7
Symon Hasan <isymonhs@gmail.com>
2020-08-20 19:08:40 +0600

    edit task through double click

--- src/Item.js
+++ src/Item.js
@@ -1,30 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { connect } from "react-redux";
 
 const Item = (props) => {
-    const [ readonly , setreadonly ] = useState( true );
-    const [ text , setText ] = useState("");
-    const [ id , setId ] = useState("");
+    const [readonly, setreadonly] = useState(true);
+    const [text, setText] = useState("");
+    const [id, setId] = useState("");
 
     useEffect(() => {
-        setText( props.text );
-        setId( props.id );
-    } , [ props.text , props.id ])
-    return(
+        setText(props.text);
+        setId(props.id);
+    }, [props.text, props.id]);
+
+    return (
         <span className="paper-item">
-            {
-                readonly ? (
-                    <input type="text" value={text} disabled />
-                ) : (
-                    <input type="text" value={text} />
-                )
-            }
+            <input
+                type="text"
+                value={text}
+                readOnly={readonly}
+                id={id}
+                onDoubleClick={(e) => {
+                    setreadonly(false);
+                }}
+                onChange={(e) => {
+                    setText(e.target.value);
+                }}
+                onKeyPress={(e) => {
+                    if( e.key === 'Enter' ){
+                        props.editTask( e.target.id , text );
+                        setreadonly( true );
+                    }
+                }}
+            />
             <div className="paper-item-icons">
-                {props.active ? <span className="tick-icon" id={id}>&#10003;</span> : null }
-                <span className="remove-icon" id={id}>x</span>
+                {props.active ? (
+                    <span className="tick-icon" id={id}>
+                        &#10003;
+                    </span>
+                ) : null}
+                <span className="remove-icon" id={id}>
+                    x
+                </span>
             </div>
         </span>
-    )
+    );
+};
+
+const mapDispatchToProps = ( dispatch ) => {
+    return{
+        editTask: ( id , text ) => {
+            dispatch({
+                type: "EDIT_TASK",
+                payload: {
+                    id: id,
+                    text: text,
+                }
+            })
+        }
+    }
 }
 
-export default Item;
+export default connect( null , mapDispatchToProps )( Item );
--- src/reducer.js
+++ src/reducer.js
@@ -1,22 +1,32 @@
 const initialState = {
     tasks: [],
-    count: 0
-}
+    count: 0,
+};
 
-const reducer = ( state=initialState , action ) => {
-    switch( action.type ){
-        case "NEW_TASK":
-            const { tasks } = state;
-            tasks.unshift({
-                ...action.payload
-            })
-            return{
-                tasks: [...tasks],
-                count: state.count + 1
-            }
-        default:
+const reducer = (state = initialState, action) => {
+    if (action.type === "NEW_TASK") {
+        const { tasks } = state;
+        tasks.unshift({
+            ...action.payload,
+        });
+        return {
+            tasks: [...tasks],
+            count: state.count + 1,
+        };
+    } else if (action.type === "EDIT_TASK") {
+        const { tasks } = state;
+        const { id , text } = action.payload;
+        tasks.splice( id , 1 );
+        tasks.splice( id , 0 , {
+            text: text,
+            status: true,
+        });
+        return{
+            ...state,
+            tasks: [...tasks],
+        }
     }
     return state;
-}
+};
 
 export default reducer;
