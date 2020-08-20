0c8b42ee3b3e85b9c282e75f27e791b1f602c477
Symon Hasan <isymonhs@gmail.com>
2020-08-20 21:04:58 +0600

    Updates

---
+++ precache-manifest.b76e7ccf7979bc08d533a5e034b99e6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8836cd1f9311cb788d9c7f20d17176c6",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "fbe1effd3df4cd53f515",
+    "url": "/todos/static/css/main.aecaee32.chunk.css"
+  },
+  {
+    "revision": "1f2ddae53496884bc8da",
+    "url": "/todos/static/js/2.78b68c15.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todos/static/js/2.78b68c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe1effd3df4cd53f515",
+    "url": "/todos/static/js/main.645b2c99.chunk.js"
+  },
+  {
+    "revision": "06f352b65e540bf1c983",
+    "url": "/todos/static/js/runtime-main.c409f8d6.js"
+  }
+]);
